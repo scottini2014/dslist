9b9ba6b5aa8688ca09bba7b8e68f77f9db86b065
Eduardo Scottini <eduardo.scottini@outlook.com>
2023-07-11 21:36:04 -0300

    Get game by id

--- src/main/java/com/scottinieduardo/dslist/services/GameService.java
+++ src/main/java/com/scottinieduardo/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.scottinieduardo.dslist.dto.GameDTO;
 import com.scottinieduardo.dslist.dto.GameMinDTO;
 import com.scottinieduardo.dslist.entities.Game;
 import com.scottinieduardo.dslist.repositories.GameRespository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRespository gameRepository;
 	
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
